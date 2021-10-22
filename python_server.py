094191afc2853b5dfaf30e54addbed8e7dc229a1
Li <Hansson.Li@Honeywell.com>
2021-10-22 16:17:13 +0800

    add multi-thread method to handle concurrency

--- python_server.py
+++ python_server.py
@@ -1,8 +1,11 @@
 #-*- coding:utf-8 -*-
-import http.server
+import time
+import threading
+from socketserver import ThreadingMixIn
+from http.server import ThreadingHTTPServer, CGIHTTPRequestHandler
 
 # class RequestHandler(http.server.BaseHTTPRequestHandler):
-class RequestHandler(http.server.CGIHTTPRequestHandler):
+class RequestHandler(CGIHTTPRequestHandler):
     '''处理请求并返回页面'''
 
     # 页面模板
@@ -20,7 +23,8 @@ class RequestHandler(http.server.CGIHTTPRequestHandler):
 
     # 处理一个GET请求
     def do_GET(self):
-        
+        message = threading.currentThread().getName()
+        print(message)
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         if "test.txt" in self.requestline:
@@ -40,21 +44,22 @@ class RequestHandler(http.server.CGIHTTPRequestHandler):
             self.send_header("Content-Length", str(len(self.Page)))
             self.end_headers()
             self.wfile.write(self.Page.encode('utf-8'))
+        time.sleep(30)
 
     def do_POST(self):
-        self.send_response(200)
-        print(self.rfile)
+        post_buf = self.rfile.read()
+        print(post_buf.decode())
         with open("test.txt", "a+") as f:
-            f.write(self.rfile)
+            f.write(post_buf.decode())
             f.close()
         f = open("test.txt","r")
         read_buf = f.read()
         print(read_buf)
         f.close()
+        self.send_response(200)
 
-#----------------------------------------------------------------------
 
 if __name__ == '__main__':
-    serverAddress = ('', 8080)
-    server = http.server.HTTPServer(serverAddress, RequestHandler)
+    serverAddress = ('', 9000)
+    server = ThreadingHTTPServer(serverAddress, RequestHandler)
     server.serve_forever()
