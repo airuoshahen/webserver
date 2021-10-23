5b47ab84fc3343026e82822802d0a08300ad44bc
airuoshahen <245285525@qq.com>
2021-10-23 17:49:17 +0800

    add code of handling exception and lock to protect file from multi-read/multi-write

--- python_server.py
+++ python_server.py
@@ -4,6 +4,7 @@ import threading
 from socketserver import ThreadingMixIn
 from http.server import ThreadingHTTPServer, CGIHTTPRequestHandler
 
+txt_mu = threading.Lock()
 # class RequestHandler(http.server.BaseHTTPRequestHandler):
 class RequestHandler(CGIHTTPRequestHandler):
     '''处理请求并返回页面'''
@@ -20,43 +21,47 @@ class RequestHandler(CGIHTTPRequestHandler):
         </body>
         </html>
     '''
-
     # 处理一个GET请求
     def do_GET(self):
         message = threading.currentThread().getName()
         print(message)
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
-        if "test.txt" in self.requestline:
-            # with open('test.txt', 'a+') as f:
-            #     f.write("test")
-            #     read_buf = f.read()
-            #     print(read_buf)
-            #     f.close()
-            f = open("test.txt","r")
-            read_buf = f.read()
-            print(read_buf)
-            f.close()
+        if "test.txt" in self.path:
+            if txt_mu.acquire(True):
+                f = open("test.txt","r")
+                read_buf = f.read()
+                print(read_buf)
+                f.close()
+                txt_mu.release()
             self.send_header("Content-Length", str(len(read_buf)))
             self.end_headers()
-            self.wfile.write(read_buf.encode('utf-8'))
+            try:
+                self.wfile.write(read_buf.encode('utf-8'))
+            except:
+                self.send_error(404, "Page not Found!")
         else:
             self.send_header("Content-Length", str(len(self.Page)))
             self.end_headers()
-            self.wfile.write(self.Page.encode('utf-8'))
+            try:
+                self.wfile.write(self.Page.encode('utf-8'))
+            except:
+                self.send_error(404, "Page not Found!")
 
     def do_POST(self):
         print(self.headers)
         print(self.command)
         read_buf = self.rfile.read(int(self.headers['content-length']))
         print(read_buf.decode())
-        with open("test.txt", "a+") as f:
-            f.write(read_buf.decode())
+        if txt_mu.acquire(True):
+            with open("test.txt", "a+") as f:
+                f.write(read_buf.decode())
+                f.close()
+            f = open("test.txt","r")
+            read_buf = f.read()
+            print(read_buf)
             f.close()
-        f = open("test.txt","r")
-        read_buf = f.read()
-        print(read_buf)
-        f.close()
+            txt_mu.release()
         # data = {
         #     'result_code': '2',
         #     'result_desc': 'Success',
